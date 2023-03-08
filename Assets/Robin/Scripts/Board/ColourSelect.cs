b57e577a0eb545e2c486cf83644e7ffc6e44cc5c
RobinKocken <robin.a.kocken@gmail.com>
2023-03-08 20:30:47 +0100

    Button

--- Assets/Robin/Scripts/Board/ColourSelect.cs
+++ Assets/Robin/Scripts/Board/ColourSelect.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ColourSelect : MonoBehaviour
@@ -7,8 +8,46 @@ public class ColourSelect : MonoBehaviour
     public GameManager gameManager;
     public GameManager.PalletColour pallet;
 
+    public float speed;
+    public float height;
+    float startFloat;
+    public float intervals;
+
+    bool buttonActivate;
+    float time;
+    Vector3 pos;
+
+    void Start()
+    {
+        pos = transform.position;
+        startFloat = transform.position.x;
+    }
+
+    void Update()
+    {
+        Button();
+    }
+
+    void Button()
+    {
+        if(buttonActivate)
+        {
+            time += intervals * Time.deltaTime;
+
+            float newFloat = startFloat + height * ((Mathf.Sin(time * speed) + 1) / 2);
+            transform.position = new Vector3(newFloat, transform.position.y, transform.position.z);
+
+            if(transform.position == pos && time > 0.05f)
+            {
+                time = 0;
+                buttonActivate = false;
+            }
+        }
+    }
+
     public void Select()
     {
         gameManager.SelectColour(pallet);
+        buttonActivate = true;
     }
 }
